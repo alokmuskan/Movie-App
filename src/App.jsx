554a8a6cbb74ee1c7a995ea6408cf6c4bd579fb5
alok raj <alokraj1319@gmail.com>
2026-01-20 21:06:53 +0530

    starting building movie app once again from beginning after learning and revising important concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
-import { useState, useEffect } from "react";
-
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect( () => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  }, [hasLiked]);
-
-  return (
-    <div className="card" onClick={() => setCount(count + 1)}>
-      <h2>{title} <br /> {count || null}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "Liked" : "Like"}
-      </button>
-
-    </div>
-  )
-}
+import React from "react"; 
 
 const App = () => {
-
   return (
-    <div className="card-container">
-      <Card title="Star wars" rating={5} isCool={true} />
-      <Card title="Avatar" />
-      <Card title="The Lion King" />
-    </div>  
+    <div>App</div>
   )
 }
-
-export default App    
