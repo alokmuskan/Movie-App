474fdba95d210ec1c0a52e07119d5196e6a0a49a
alok raj <alokraj1319@gmail.com>
2026-01-20 21:14:57 +0530

    installed tailwind css for styling and styled some of the components that we are going to use in building the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
+
+export default App;
